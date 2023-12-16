9502100a8681121356079dc1afef71ee5ebcafa0
Jrogo <>
2023-12-16 15:52:32 -0500

    Formatted config file

--- VenueManager/Configuration.cs
+++ VenueManager/Configuration.cs
@@ -5,31 +5,32 @@ using System.Collections.Generic;
 
 namespace VenueManager
 {
-    [Serializable]
-    public class Configuration : IPluginConfiguration
+
+  [Serializable]
+  public class Configuration : IPluginConfiguration
+  {
+    public int Version { get; set; } = 0;
+    // Should chat message alerts be printed to the chat
+    public bool showChatAlerts { get; set; } = false;
+    public bool showPluginNameInChat { get; set; } = false;
+    // Should sound alerts be played when new players join the house 
+    public bool soundAlerts { get; set; } = false;
+    public float soundVolume { get; set; } = 1;
+    // Territory that the current user is in
+    public ushort territory { get; set; } = 0;
+
+    // the below exist just to make saving less cumbersome
+    [NonSerialized]
+    private DalamudPluginInterface? pluginInterface;
+
+    public void Initialize(DalamudPluginInterface pluginInterface)
     {
-        public int Version { get; set; } = 0;
-        // Should chat message alerts be printed to the chat
-        public bool showChatAlerts { get; set; } = false; 
-        public bool showPluginNameInChat { get; set; } = false; 
-        // Should sound alerts be played when new players join the house 
-        public bool soundAlerts { get; set; } = false; 
-        public float soundVolume { get; set; } = 1; 
-        // Territory that the current user is in
-        public ushort territory { get; set; } = 0;
-
-        // the below exist just to make saving less cumbersome
-        [NonSerialized]
-        private DalamudPluginInterface? pluginInterface;
-
-        public void Initialize(DalamudPluginInterface pluginInterface)
-        {
-            this.pluginInterface = pluginInterface;
-        }
-
-        public void Save()
-        {
-            this.pluginInterface!.SavePluginConfig(this);
-        }
+      this.pluginInterface = pluginInterface;
     }
+
+    public void Save()
+    {
+      this.pluginInterface!.SavePluginConfig(this);
+    }
+  }
 }
