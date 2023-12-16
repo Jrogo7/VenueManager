37cde19e91d2ff466274f229dcd03437d12573ab
Jrogo <>
2023-12-16 03:19:17 -0500

    Disabled alerts by default

--- ClubManager/Configuration.cs
+++ ClubManager/Configuration.cs
@@ -10,9 +10,9 @@ namespace ClubManager
     {
         public int Version { get; set; } = 0;
         // Should chat message alerts be printed to the chat
-        public bool showChatAlerts { get; set; } = true; 
+        public bool showChatAlerts { get; set; } = false; 
         // Should sound alerts be played when new players join the house 
-        public bool soundAlerts { get; set; } = true; 
+        public bool soundAlerts { get; set; } = false; 
         public float soundVolume { get; set; } = 1; 
         // Territory that the current user is in
         public ushort territory { get; set; } = 0;
